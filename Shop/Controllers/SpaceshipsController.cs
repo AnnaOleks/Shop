a5ea47cfecabf00f0b93f65262fe374b521393fc
Anna Oleks <annaoleks88@gmail.com>
2025-09-02 19:27:53 +0300

    1 tund

---
+++ Shop/Controllers/SpaceshipsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shop.Models.Spaceships;
+using Shop.Data;
+
+
+namespace Shop.Controllers
+{
+    public class SpaceshipsController : Controller
+    {
+        private readonly ShopContext _context;
+        public SpaceshipsController
+            (
+                ShopContext context
+            )
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var result = _context.Spaceships
+                .Select(x => new SpaceshipsIndexViewModel
+                {
+                    ID = x.ID,
+                    Name = x.Name,
+                    BuiltDate = x.BuiltDate,
+                    TypeName = x.TypeName
+                });
+
+            return View(result);
+        }
+    }
+}
---
+++ Shop/Models/Spaceships/SpaceshipsIndexViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Shop.Models.Spaceships
+{
+    public class SpaceshipsIndexViewModel
+    {
+        public Guid? ID { get; set; }
+        public string? Name { get; set; }
+        public string? TypeName { get; set; }
+        public DateTime? BuiltDate { get; set; }
+        public int? Crew { get; set; }
+        public int? EnginePower { get; set; }
+        public int? Passengers { get; set; }
+        public int? InnerVolume  { get; set; }
+    }
+}
