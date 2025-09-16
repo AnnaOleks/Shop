70a83048c94022827c7ab95196f7f7362cf753da
Anna Oleks <annaoleks88@gmail.com>
2025-09-16 18:29:56 +0300

    KindergardenDto

--- Shop.Core/Domain/Kindergarden.cs
+++ Shop.Core/Domain/Kindergarden.cs
@@ -1,20 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Shop.Core.Domain
+﻿namespace Shop.Core.Domain
 {
     public class Kindergarden
     {
-        public Guid Id { get; set; }
-        public string GroupName { get; set; }
-        public int ChildrenCount { get; set; }
-        public string KindergardenName { get; set; }
-        public string TeacherName { get; set; }
-        public DateTime CreatedAt { get; set; }
-        public DateTime UpdatedAt { get; set; }
+        public Guid? Id { get; set; }
+        public string? GroupName { get; set; }
+        public int? ChildrenCount { get; set; }
+        public string? KindergardenName { get; set; }
+        public string? TeacherName { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
 
         //Teha uus branch nimega Kindergarden
         //Teha uus CRUD lasamine, vaatamine, uuendamine ja andmete kustutamine.
