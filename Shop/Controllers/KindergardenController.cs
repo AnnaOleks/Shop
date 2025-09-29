78ee18299c18783e85bf202acd9b57501390258e
Anna Oleks <annaoleks88@gmail.com>
2025-09-29 22:27:41 +0300

    KindergardenIndexViewModel

--- Shop/Controllers/KindergardenController.cs
+++ Shop/Controllers/KindergardenController.cs
@@ -1,6 +1,41 @@
-﻿namespace Shop.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Shop.Core.Domain;
+using Shop.Core.ServiceInterface;
+using Shop.Data;
+using Shop.Models.Kindergarden;
+using Shop.Models.Spaceships;
+
+namespace Shop.Controllers
 {
-    public class KindergardenController
+    public class KindergardenController : Controller
     {
+        private readonly ShopContext _context;
+        private readonly IKindergardenServices _kindergardenServices;
+        public KindergardenController
+            (
+                ShopContext context,
+                IKindergardenServices kindergardenServices
+            )
+        {
+            _context = context;
+            _kindergardenServices = kindergardenServices;
+        }
+
+        public IActionResult Index()
+        {
+            var result = _context.Kindergardens
+                .Select(x => new KindergardenIndexViewModel
+                {
+                    Id = x.Id,
+                    GroupName = x.GroupName,
+                    ChildrenCount = x.ChildrenCount,
+                    KindergardenName = x.KindergardenName,
+                    TeacherName = x.TeacherName
+                });
+
+            return View(result);
+        }
+        
     }
 }
