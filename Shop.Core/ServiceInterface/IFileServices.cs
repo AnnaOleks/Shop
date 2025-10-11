686d9df9e2991944952dae4ee2761ffd5a386b9b
Anna Oleks <annaoleks88@gmail.com>
2025-10-11 16:55:49 +0300

    IFileServices

--- Shop.Core/ServiceInterface/IFileServices.cs
+++ Shop.Core/ServiceInterface/IFileServices.cs
@@ -1,10 +1,12 @@
 ﻿using Shop.Core.Domain;
 using Shop.Core.Dto;
+using System.Xml;
 
 namespace Shop.Core.ServiceInterface
 {
     public interface IFileServices
     {
         void FilesToApi(SpaceshipDto dto, Spaceship spaceship);
+        void UploadFilesToDatabase(KindergardenDto dto, Kindergarden domain);
     }
 }
