dad2f44f1caada92616216b0256e5bedfa3a9447
Anna Oleks <annaoleks88@gmail.com>
2025-10-14 18:09:27 +0300

    AccuCityCodeRootDto

---
+++ Shop.Core/Dto/AccuLocationWeatherResultDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Core.Dto
+{
+    public class AccuLocationWeatherResultDto
+    {
+        public string? CityName { get; set; } = string.Empty;
+    }
+}
---
+++ Shop/Controllers/WeatherController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shop.ApplicationServices.Services;
+using Shop.Core.Dto;
+
+namespace Shop.Controllers
+{
+    public class WeatherController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SearchCity(AccuWeatherSearchViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("City", "Weather", new { city = model.CityName });
+            }
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult City(string city)
+        {
+            AccuLocationWeatherResultDto dto = new();
+            dto.CityName = city;
+
+            _weatherForecastServices.AccuWeatherResult(dto);
+            return View(dto);
+        }
+    }
+}
