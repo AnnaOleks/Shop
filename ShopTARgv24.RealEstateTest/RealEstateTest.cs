caa32efcc3b753276b39dfffa9fe2e2ae5c766db
Anna Oleks <annaoleks88@gmail.com>
2025-11-03 16:14:29 +0200

    /

---
+++ ShopTARgv24.RealEstateTest/RealEstateTest.cs
@@ -0,0 +1,26 @@
+﻿using Shop.Core.Dto;
+using Shop.Core.ServiceInterface;
+using System.Threading.Tasks;
+
+namespace ShopTARgv24.RealEstateTest
+{
+    public class RealEstateTest : TestBase
+    {
+        [Fact]
+        public async Task Test1()
+        {
+            RealEstateDto dto = new();
+
+            dto.Area = 120.5;
+            dto.Location = "Downtown";
+            dto.RoomNumber = 3;
+            dto.BuildingType = "Apartment";
+            dto.CreatedAt = DateTime.Now;
+            dto.ModifiedAt = DateTime.Now;
+
+            var result = await Svc<IRealEstateService>().Create(dto);  
+
+            Assert.NotNull(result);
+        }
+    }
+}
---
+++ ShopTARgv24.RealEstateTest/TestBase.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+
+namespace ShopTARgv24.RealEstateTest
+{
+    public abstract class TestBase
+    {
+        //
+        protected IServiceProvider serviceProvider { get; set; }
+
+        protected TestBase()
+        {
+            var services = new ServiceCollection();
+            SetupServices(services);
+            serviceProvider = services.BuildServiceProvider();
+        }
+
+        public virtual void SetupServices(IServiceCollection services)
+        {
+            services.AddDbContext<ShopContext>(X =>
+            {
+                X.UseInMemoryDatabase("TestDb");
+                X.ConfigureWarnings(b => b.Ignore(InMemoryEventId.TransactionIgnoredWarning));
+            });
+        }
+
+        private void RegisterMacros(IServiceCollection services)
+        {
+        }
+
+        protected T Svc<T>()
+        {
+            return serviceProvider.GetService<T>();
+        }
+    }
+}
