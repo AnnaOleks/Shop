16f2c32bc4fb9b1da5a96f71a814032250dfa2d4
Anna Oleks <annaoleks88@gmail.com>
2025-11-09 18:09:00 +0200

    .

--- ShopTARgv24.RealEstateTest/TestBase.cs
+++ ShopTARgv24.RealEstateTest/TestBase.cs
@@ -1,6 +1,12 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Shop.Data;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Diagnostics;
+using ShopTARgv24.RealEstateTest.Macros;
+using Shop.Core.ServiceInterface;
+using Shop.ApplicationServices.Services;
+using Microsoft.Extensions.Hosting;
+using ShopTARgv24.RealEstateTest.Mock;
 
 namespace ShopTARgv24.RealEstateTest
 {
@@ -18,20 +24,39 @@ namespace ShopTARgv24.RealEstateTest
 
         public virtual void SetupServices(IServiceCollection services)
         {
-            services.AddDbContext<ShopContext>(X =>
+
+            services.AddScoped<IRealEstateService, RealEstateServices>();
+
+            services.AddScoped<IFileServices, FileServices>();
+            services.AddScoped<IHostEnvironment, MockHostEnvironment>();
+
+            services.AddSingleton<IHostEnvironment>(new TestHostEnvironment());
+
+            services.AddDbContext<ShopContext>(x =>
             {
-                X.UseInMemoryDatabase("TestDb");
-                X.ConfigureWarnings(b => b.Ignore(InMemoryEventId.TransactionIgnoredWarning));
+                //
+                x.UseInMemoryDatabase("TestDb");
+                //
+                x.ConfigureWarnings(b => b.Ignore(InMemoryEventId.TransactionIgnoredWarning));
             });
+            RegisterMacros(services);
         }
 
         private void RegisterMacros(IServiceCollection services)
         {
+            var macroBaseType = typeof(IMacros);
+            var macroTypes = macroBaseType.Assembly.GetTypes()
+                .Where(t => macroBaseType.IsAssignableFrom(t)
+                && t.IsInterface && !t.IsAbstract);
         }
 
         protected T Svc<T>()
         {
             return serviceProvider.GetService<T>();
+        }
+
+        public void Dispose()
+        {
         }
     }
 }
---
+++ ShopTARgv24.RealEstateTest/TestHostEnvironment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO; // Добавьте для Directory.GetCurrentDirectory()
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.FileProviders; // !!! НУЖНЫЙ USING !!!
+
+namespace ShopTARgv24.RealEstateTest
+{
+    public class TestHostEnvironment : IHostEnvironment // Если нужна полная реализация
+    {
+        public string EnvironmentName { get; set; } = "Testing";
+        public string ApplicationName { get; set; } = "ShopTARgv24";
+        public string ContentRootPath { get; set; } = Directory.GetCurrentDirectory();
+
+        // !!! ИСПРАВЛЕНО: Тип должен быть IFileProvider !!!
+        public IFileProvider ContentRootFileProvider { get; set; } = default!;
+
+        // Эти свойства нужны, если FileServices на самом деле требует IWebHostEnvironment
+        // public string WebRootPath { get; set; } = default!; 
+        // public IFileProvider WebRootFileProvider { get; set; } = default!;
+    }
+}
